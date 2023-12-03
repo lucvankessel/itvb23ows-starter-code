07b621c169552b20e159f64813de005df85ff728
Luc van Kessel <lucv.k@outlook.com>
2023-12-03 17:43:45 +0100

    fixed a bug in php that complained about not being able to pass non-variables by refference

--- play.php
+++ play.php
@@ -26,8 +26,9 @@ elseif (array_sum($hand) <= 8 && $hand['Q']) {
     $_SESSION['hand'][$player][$piece]--;
     $_SESSION['player'] = 1 - $_SESSION['player'];
     $db = include 'database.php';
+    $state = get_state();
     $stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
-    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], get_state());
+    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], $state);
     $stmt->execute();
     $_SESSION['last_move'] = $db->insert_id;
 }
