cc825a346dc96f9d86439f94dc940aa4e4f0bb51
Luc van Kessel <lucv.k@outlook.com>
2023-12-08 11:06:20 +0100

    updated restart.php to work correctly with PDO

--- restart.php
+++ restart.php
@@ -7,8 +7,9 @@ $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 
 $_SESSION['player'] = 0;
 
 $db = include_once 'database.php';
-$db->prepare('INSERT INTO games VALUES ()')->execute();
-$_SESSION['game_id'] = $db->insert_id;
+$stmt = $db->prepare('INSERT INTO games VALUES ()')->execute();
+$result = $db->lastInsertId();
+$_SESSION['game_id'] = $result;
 
 header('Location: index.php');
 
